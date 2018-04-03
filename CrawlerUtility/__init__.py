349798251f6dc56a778a8708684946d780234b6a
xieqiankun <xieqk@zenme.com>
2018-04-03 13:47:09 +0800

    prepare for upload to pypi

--- CrawlerUtility/__init__.py
+++ CrawlerUtility/__init__.py
@@ -4,5 +4,4 @@ CrawlerUtility.py
 This is a utility package to simplify your development of webcrawler.
 Now it
 """
-from common_utility.ChromeHeaders2Dict import *
 __version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name='CrawlerUtility',
+  packages=['CrawlerUtility'],
+  version='0.1',
+  description='Utilities to simplify the development of your webcrawler',
+  author='Kingname',
+  author_email='contact@kingname.info',
+  url='https://github.com/kingname/CrawlerUtility', # use the URL to the github repo
+  keywords=['scrapy', 'requests', 'crawler'],
+  classifiers=[
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ]
+)
