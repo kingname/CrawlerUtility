352451d7520d6e4f7989e3ba1e32fa4633e5efcb
xieqiankun <xieqk@zenme.com>
2018-04-03 14:00:59 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
-  name='CrawlerUtility',
-  packages=['CrawlerUtility'],
-  version='0.1',
-  description='Utilities to simplify the development of your webcrawler',
-  author='Kingname',
-  author_email='contact@kingname.info',
-  url='https://github.com/kingname/CrawlerUtility', # use the URL to the github repo
-  keywords=['scrapy', 'requests', 'crawler'],
-  classifiers=[
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ]
+    name='CrawlerUtility',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Utilities to simplify the development of your webcrawler',
+    author='Kingname',
+    author_email='contact@kingname.info',
+    url='https://github.com/kingname/CrawlerUtility',
+    keywords=['scrapy', 'requests', 'crawler'],
+    python_requires='>=3.4',
+    license='PyPA',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'License :: PyPA',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+    ]
 )
